7ae0a599253d1cb5b2f6fe3a4970b85aaf4de369
kantlock <dogan-sait@hotmail.com>
2020-01-05 01:10:14 +0300

    Register Activity request, ResponseMessage class added

--- app/src/main/java/com/sbc/childtracker/RegisterActivity.java
+++ app/src/main/java/com/sbc/childtracker/RegisterActivity.java
@@ -5,12 +5,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.sbc.childtracker.requests.CustomRequest;
+import com.sbc.childtracker.requests.ResponseMessage;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
   private EditText editText_password;
   private Button button_register;
 
+  private static final String serverAddress = "http://192.168.1.23:3000";
   private static final String TAG = RegisterActivity.class.getSimpleName();
 
   @Override
@@ -43,30 +54,52 @@ public class RegisterActivity extends AppCompatActivity {
     button_register.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        User user = new User(editText_name.getText().toString(),
-                editText_surname.getText().toString(),
-                editText_phone.getText().toString(),
-                editText_email.getText().toString(),
-                editText_password.getText().toString());
 
-        //TODO: SEND USER INFORMATION TO SERVER
+        makeRegisterRequest();
       }
     });
   }
 
-  @Getter
-  @Setter
-  @AllArgsConstructor
-  private class User {
-    private String name;
-    private String surname;
-    private String phone;
-    private String email;
-    private String password;
+  private void makeRegisterRequest() {
+    String url = serverAddress + "/api/register";
 
-    @Override
-    public String toString() {
-      return name + " / " + surname + " / " + phone + " / " + email + " / " + password;
-    }
+    Map<String, String> params = new HashMap<>();
+
+    params.put("name", editText_name.getText().toString());
+    params.put("surname", editText_surname.getText().toString());
+    params.put("phone", editText_phone.getText().toString());
+    params.put("email", editText_email.getText().toString());
+    params.put("password", editText_password.getText().toString());
+
+    Response.Listener<JSONObject> successListener = new Response.Listener<JSONObject>() {
+      @Override
+      public void onResponse(JSONObject response) {
+        Log.d(TAG, "Successful:\t" + response.toString());
+
+        showSuccessMessage();
+      }
+    };
+
+    Response.ErrorListener errorListener = new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        Log.e(TAG, "Error:\t" + error.getMessage());
+
+        showFailedMessage();
+      }
+    };
+
+    CustomRequest customRequest = new CustomRequest(Request.Method.POST, url, params, successListener, errorListener);
+    RequestQueue queue = Volley.newRequestQueue(this);
+
+    queue.add(customRequest);
+  }
+
+  private void showSuccessMessage(){
+    Toast.makeText(getApplicationContext(), "Successfully registered", Toast.LENGTH_LONG).show();
+  }
+
+  private void showFailedMessage(){
+    Toast.makeText(getApplicationContext(), "Error while registering, please try again.", Toast.LENGTH_LONG).show();
   }
 }
